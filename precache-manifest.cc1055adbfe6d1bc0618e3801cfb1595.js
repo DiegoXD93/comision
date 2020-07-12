42097ceff16af5628e073686f951c79ae5e72dd7
diegoXD93 <bolivaristadiego@gmail.com>
2020-07-11 20:52:07 -0400

    Updates

---
+++ precache-manifest.cc1055adbfe6d1bc0618e3801cfb1595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c429e2ab1fd84d0c64e4f42a9248d96b",
+    "url": "/comision/index.html"
+  },
+  {
+    "revision": "7f567ff2419cecd42952",
+    "url": "/comision/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/comision/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comision/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f567ff2419cecd42952",
+    "url": "/comision/static/js/main.d64ad92a.chunk.js"
+  },
+  {
+    "revision": "5572d3a01b97d37bd8f8",
+    "url": "/comision/static/js/runtime-main.86d0fcd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comision/static/media/logo.5d5d9eef.svg"
+  }
+]);
